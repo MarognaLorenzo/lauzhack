e7941ed36e8ca37e268aaec10aa665fd8494b2c2
marelaG <marcelina.gancewska@student.kuleuven.be>
2025-11-22 15:29:13 +0100

    add button

--- app/src/main/java/com/example/lauzhack/MainActivity.kt
+++ app/src/main/java/com/example/lauzhack/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.example.lauzhack
 
 import android.os.Bundle
+import android.util.Log // Import for logging
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size // Import for size
+import androidx.compose.material3.Button // Import for Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment // Import for alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp // Import for size units
+import androidx.compose.ui.unit.sp // Import for font size
 import com.example.lauzhack.ui.theme.LauzHackTheme
+
+// Define a TAG for the Logcat messages
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +32,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             LauzHackTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    // Change the content to use the new ScreenWithButton composable
+                    ScreenWithButton(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +43,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ScreenWithButton(modifier: Modifier = Modifier) {
+    // Column to center the button on the screen
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // The Big Button
+        Button(
+            onClick = {
+                // This is where the log message is written to the console (Logcat)
+                Log.d(TAG, "Big Button was clicked!")
+            },
+            // Make the button large
+            modifier = Modifier
+                .fillMaxWidth(0.8f) // 80% of the screen width
+                .padding(24.dp)
+                .size(width = 300.dp, height = 100.dp) // Explicit size for "big" effect
+        ) {
+            Text(
+                text = "CLICK ME",
+                fontSize = 28.sp // Larger text for a big button
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScreenWithButtonPreview() {
     LauzHackTheme {
-        Greeting("Android")
+        ScreenWithButton()
     }
 }
